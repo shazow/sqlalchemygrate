b1fdb425b2ed10b8151bf259582a17961b34a85d
Andrey Petrov <andrey.petrov@shazow.net>
2011-06-06 23:09:37 -0700

    Bad return, should be continue.

--- sqlalchemygrate/migrations.py
+++ sqlalchemygrate/migrations.py
@@ -161,8 +161,9 @@ def migrate(e1, e2, metadata, convert_map=None, populate_fn=None, only_tables=No
             if new_table is None:
                 log.info("No corresponding table found, skipping: {0}".format(table_name))
                 continue
+
             table_migrate(e1, e2, table, new_table, limit=limit)
-            return
+            continue
 
         for new_table_name, convert_fn in convert:
             new_table = metadata.tables.get(new_table_name)
